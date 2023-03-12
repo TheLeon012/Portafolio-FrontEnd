023d119212acafd0206369beec01ae9c0785e084
theleon012 <gtheleon@gmail.com>
2023-03-12 20:59:01 -0300

    cambios de responsive en skill y proyectos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProyectosComponent } from './componentes/proyectos/proyectos.component'
 import { HomeComponent } from './componentes/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 const appRoutes: Routes=[
   {path:'', component:HomeComponent},
@@ -32,7 +33,8 @@ const appRoutes: Routes=[
     ExperienciaComponent,
     ProyectosComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortafolioService } from 'src/app/servicios/portafolio.service';
+
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
-export class ProyectosComponent {
+export class ProyectosComponent implements OnInit {
+
+  ProyecLista: any;
+  constructor(private datosPorfolio:PortafolioService){}
+
+  ngOnInit(): void{
+    this.datosPorfolio.obtenerDatos().subscribe(data=>{
+      this.ProyecLista=data.proyectosJson;
+    })
+  }
 
 }
